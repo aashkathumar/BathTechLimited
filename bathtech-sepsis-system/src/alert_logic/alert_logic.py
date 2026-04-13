1bc302fd6e5a6d2854efb6bcc25b357eb693922f
at3148 <at3148@bath.ac.uk>
2026-04-13 01:54:39 +0100

    updated name of teh file

--- bathtech-sepsis-system/src/alert_logic/alert_logic.py
+++ bathtech-sepsis-system/src/alert_logic/alert_logic.py
@@ -1,6 +1,9 @@
+# BathTech Limited - Sepsis alert logic
+
+
 def check_sepsis(heart_rate: float, temperature: float) -> bool:
     """
     Returns True if patient vitals indicate possible sepsis.
     Criteria: heart rate > 90 bpm AND temperature > 38.3C
     """
-    return heart_rate > 90 and temperature > 38.3   
+    return heart_rate > 90 and temperature > 38.3
