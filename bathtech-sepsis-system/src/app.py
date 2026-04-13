9537e58e7dc600ecc11db2da0197bc3206e76fa1
at3148 <at3148@bath.ac.uk>
2026-04-13 01:14:50 +0100

    fix: flake8 issues

--- bathtech-sepsis-system/src/app.py
+++ bathtech-sepsis-system/src/app.py
@@ -4,19 +4,19 @@ from alert_logic.alerts import generate_alert
 
 app = Flask(__name__)
 
+
 @app.route("/health")
 def health():
     return jsonify({"status": "ok"})
+
 
 @app.route("/predict")
 def predict():
     score = predict_sepsis_score(110, 39)
     alert = generate_alert(score)
 
-    return jsonify({
-        "score": score,
-        "alert": alert
-    })
+    return jsonify({"score": score, "alert": alert})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
