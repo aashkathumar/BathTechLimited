027627ab053d7d7df0209776b57cdded228919f6
at3148 <at3148@bath.ac.uk>
2026-04-13 02:52:40 +0100

    formatting issues fixed

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from alert_logic.alerts import generate_alert
+from sepsis_engine.model import predict_sepsis_score 
 
 app = Flask(__name__)
 
@@ -9,12 +10,11 @@ def health():
 
 @app.route("/predict")
 def predict():
-    # Example logic: in a real system, these would come from patient sensors
     hr, temp = 110, 39.0
     is_sepsis = generate_alert(hr, temp)
     return jsonify({"sepsis_alert": is_sepsis})
 
 
-# <--- MUST HAVE TWO BLANK LINES HERE
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+    
