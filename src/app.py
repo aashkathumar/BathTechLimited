f7fa49f58df1b5a479c524b16e4bd0b3060e0e83
at3148 <at3148@bath.ac.uk>
2026-04-13 03:18:15 +0100

    new logic for ci

--- src/app.py
+++ src/app.py
@@ -1,6 +1,33 @@
+# from flask import Flask, jsonify
+# from alert_logic.alerts import generate_alert
+# from sepsis_engine.model import predict_sepsis_score
+
+# app = Flask(__name__)
+
+
+# @app.route("/health")
+# def health():
+#     return jsonify({"status": "ok"})
+
+
+# @app.route("/predict")
+# def predict():
+#     hr, temp = 110, 39.0
+#     # Using the import to fix the F401 error
+#     score = predict_sepsis_score(hr, temp)
+#     is_sepsis = generate_alert(hr, temp)
+#     return jsonify({
+#         "sepsis_alert": is_sepsis,
+#         "risk_score": score
+#     })
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
+
+
 from flask import Flask, jsonify
 from alert_logic.alerts import generate_alert
-from sepsis_engine.model import predict_sepsis_score
 
 app = Flask(__name__)
 
@@ -13,13 +40,8 @@ def health():
 @app.route("/predict")
 def predict():
     hr, temp = 110, 39.0
-    # Using the import to fix the F401 error
-    score = predict_sepsis_score(hr, temp)
     is_sepsis = generate_alert(hr, temp)
-    return jsonify({
-        "sepsis_alert": is_sepsis,
-        "risk_score": score
-    })
+    return jsonify({"sepsis_alert": is_sepsis})
 
 
 if __name__ == "__main__":
