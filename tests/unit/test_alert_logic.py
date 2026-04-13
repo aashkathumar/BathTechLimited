bbd7c8f9ac4e2c5e5ca628de20592eaca9a26a90
at3148 <at3148@bath.ac.uk>
2026-04-13 02:19:09 +0100

    json file added

--- tests/unit/test_alert_logic.py
+++ tests/unit/test_alert_logic.py
@@ -1,4 +1,4 @@
-from src.alert_logic import check_sepsis
+from alert_logic import check_sepsis
 
 
 def test_normal_patient_no_sepsis():
@@ -11,3 +11,4 @@ def test_sepsis_detected():
 
 def test_high_hr_normal_temp_no_sepsis():
     assert not check_sepsis(heart_rate=95, temperature=37.5)
+    
